b38d8f76cca9df3bca1432206b9b4f5a278be800
YLJOSE <joserojasr47@gmail.com>
2024-11-14 19:00:17 +0100

    acceso a datos y bbdd revisar codigo del canvas de bbdd xd

--- src/main/java/SubsistemaGestionUsuarios/InterfaceGrap.java
+++ src/main/java/SubsistemaGestionUsuarios/InterfaceGrap.java
@@ -5,11 +5,35 @@ import javax.swing.*;
 public class InterfaceGrap {
 
     public static void main(String[] args) {
-        String id_USER;
-       id_USER =  JOptionPane.showInputDialog("Ingrese el id de usuario:");
-       if(id_USER.isBlank()){
-        main(args);
-       }
+        login();
 
+    }
+
+    public static void login() {
+
+        String id_User, passwd;
+        id_User = JOptionPane.showInputDialog("Ingrese el id de usuario:");
+        passwd = JOptionPane.showInputDialog("Ingrese la contraseña:");
+
+
+        String a = getHash(passwd);
+        System.out.println(a);
+
+    }
+
+    // metodo para obtener el hash MD5
+    public static String getHash(String txt) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(txt.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
---
+++ src/main/java/hilos/HiloProductor.java
@@ -0,0 +1,9 @@
+package hilos;
+
+public class HiloProductor extends  Thread{
+
+    public HiloProductor(){
+
+    }
+
+}
